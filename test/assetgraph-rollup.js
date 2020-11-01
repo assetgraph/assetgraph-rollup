26fe1906b244d05fb0389dd6508b14c01bf73c52
Andreas Lind <andreaslindpetersen@gmail.com>
2020-11-01 01:06:23 +0100

    Add a rollup plugin so that rollup loads assets from the assetgraph instance instead of from disc

--- test/assetgraph-rollup.js
+++ test/assetgraph-rollup.js
@@ -17,15 +17,43 @@ describe('assetgraph-rollup', function () {
       ),
     });
 
-    const [indexHtml] = await assetGraph.loadAssets('/src/index.html');
+    const [htmlAsset] = await assetGraph.loadAssets('/src/index.html');
 
     await assetGraph.populate({ followRelations: { crossorigin: false } });
 
-    await assetgraphRollup(assetGraph, indexHtml);
+    await assetgraphRollup(assetGraph, htmlAsset);
     // FIXME: This is just for testing:
     await assetGraph.writeAssetsToDisc(
       { protocol: 'file:', isLoaded: true, isRedirect: false },
       'foo'
     );
+  });
+
+  it('should bundle assets that are not found on disc', async function () {
+    const assetGraph = new AssetGraph();
+
+    assetGraph.addAsset({
+      type: 'JavaScript',
+      url: 'https://example.com/imported.js',
+      text: 'export function foo (){console.log("Hello")}',
+    });
+
+    assetGraph.addAsset({
+      type: 'JavaScript',
+      url: 'https://example.com/entrypoint.js',
+      text: 'import { foo } from "./imported.js"; foo();',
+    });
+
+    const htmlAsset = assetGraph.addAsset({
+      type: 'Html',
+      url: 'https://example.com/index.html',
+      text:
+        '<!DOCTYPE html><script type="module" src="entrypoint.js"></script>',
+    });
+
+    await assetgraphRollup(assetGraph, htmlAsset);
+
+    const bundleJavaScript = htmlAsset.outgoingRelations[0].to;
+    expect(bundleJavaScript.text, 'to contain', 'Hello');
   });
 });
